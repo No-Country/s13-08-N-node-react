ff807198c74ee551f23bab88c9026f896d349655
rretta <caracido@gmail.com>
2024-02-16 18:42:25 -0300

    feature: add map style + sites mocked

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -3,6 +3,8 @@ import { MapContainer, TileLayer, Popup, Marker, Circle } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { getUserLocation } from '../../helpers/getUserLocation';
 
+import mapMock from '../../mocks/mapMock';
+
 const ubicaciones = [
   {
     nombre: 'EcoReciclaje',
@@ -36,23 +38,38 @@ const purpleOptions = { color: 'red' };
 
 export const Map = () => {
   const [location, setLocation] = useState([]);
+  const [map, setMap] = useState({});
 
   useEffect(() => {
     getUserLocation().then((coords) => setLocation(coords));
+    setMap(mapMock);
+    console.log(map);
   }, []);
+
+  // var OpenStreetMap_DE = L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
+  //   maxZoom: 18,
+  //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  // });
 
   return (
     <>
       <MapContainer
         className="h-[100vh] sm:h-[50vh] sm:w-3/4 mx-auto relative z-0"
-        center={{ lat: '-34.579716', lng: '-58.426167' }}
+        center={{ lat: '-34.582716', lng: '-58.426167' }}
         zoom={15}
         scrollWheelZoom={true}
       >
-        <TileLayer
+        {/* <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        /> */}
+        {/* b628c55280msheb2b2ae6c91c18bp1c71bejsncdb5e88638d6 */}
+        <TileLayer
+          attribution="OpenStreetMap DE"
+          url="https://tile.openstreetmap.de/{z}/{x}/{y}.png"
+          key="b628c55280msheb2b2ae6c91c18bp1c71bejsncdb5e88638d6"
         />
+
         {/* <Marker position={{ lat: 'longitud', lng: 'latitud' }}>
           <Popup>Tu ubicación actual.</Popup>
         </Marker> */}
@@ -63,6 +80,7 @@ export const Map = () => {
           <Popup>Tu ubicación actual.</Popup>
         </Circle>
 
+        {/* MAPEO DE UBICACIONES HARCODEADAS EN COMPONENTE */}
         {ubicaciones.map((ubicacion, index) => (
           <Marker key={index} position={[ubicacion.latLng.lat, ubicacion.latLng.lng]}>
             <Popup>
@@ -71,6 +89,24 @@ export const Map = () => {
               {/* Condición para verificar si existe ubicacion.img */}
               <span className="text-green-900 text-lg font-bold">{ubicacion.nombre}</span> <br />
               {ubicacion.direccion} <br />⌚ {ubicacion.horario} <br />
+            </Popup>
+          </Marker>
+        ))}
+
+        {/* MAPEO DE UBICACIONES MOCKEADAS CON FAKE JSON TRAIDAS DESDE mapMock.js */}
+        {mapMock.features.map((ubicacion, index) => (
+          <Marker key={index} position={[ubicacion.geometry.coordinates[1], ubicacion.geometry.coordinates[0]]}>
+            <Popup>
+              {ubicacion.img
+                ? (
+                <img src={ubicacion.geometry.coordinates[0]} alt={ubicacion.geometry.coordinates[1]} />
+                  )
+                : null}
+              <br />
+              {/* Condición para verificar si existe ubicacion.img */}
+              <span className="text-green-900 text-lg font-bold">{ubicacion.properties.cooperativ}</span> <br />
+              {ubicacion.properties.direccion} <br />⌚ {ubicacion.properties.dia_hora} <br />
+              MAS INFO: {ubicacion.properties.mas_info}
             </Popup>
           </Marker>
         ))}
