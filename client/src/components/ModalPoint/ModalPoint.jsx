f8552cd26a3d9482d2be42e7071aa063a5a4847d
jmk1988 <juanmaster23@gmail.com>
2024-02-21 22:11:19 -0300

    Creacion componente ModalMaps

--- client/src/components/ModalPoint/ModalPoint.jsx
+++ client/src/components/ModalPoint/ModalPoint.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const ModalPoint = ({ubicacionData, onClose}) => {
+const ModalPoint = ({ ubicacionData, onClose }) => {
   return (
     <>
-    <div className='modal'>
-        <div className='modal-contenido'>
-            <span className='close' onClick={onClose}>
-                $times;
-            </span>
-            <h2>{ubicacionData.nombre}</h2>
-            {ubicacionData.img && <img src='{ubicacionData.img}' alt={ubicacionData.nombre} />}
-            <p>Direccion: {ubicacionData}</p>
-            <p>Horario: {ubicacionData}</p>
-
+      <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+        <div className='bg-white p-8 max-w-md rounded-lg'>
+          <button className='absolute top-4 right-4 text-gray-600 hover:text-gray-800' onClick={onClose}>
+            &times;
+          </button>
+          <h2 className='text-2xl font-bold mb-4'>{ubicacionData.nombre}</h2>
+          {ubicacionData.img && <img className='w-full mb-4' src={ubicacionData.img} alt={ubicacionData.nombre} />}
+          <p><strong>Dirección:</strong> {ubicacionData.direccion}</p>
+          <p><strong>Horario:</strong> {ubicacionData.horario}</p>
         </div>
-    </div>
+      </div>
     </>
   );
 };
 
-export default ModalPoint
+export default ModalPoint;
