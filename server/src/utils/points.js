d8422f2f53837cb0c2c0af5b2fba47322273ebfb
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2024-02-22 22:00:34 -0300

    ok

--- server/src/utils/points.js
+++ server/src/utils/points.js
@@ -2,11 +2,11 @@
   {
       "_id": "65cf79667c7f00be703574ae",
       "nombre": "Punto de Reciclaje 1",
+      "ubicacion": "Avenida La Fontanera 865",
       "imagen": "https://ejemplo.com/imagen.jpg",
       "latLng": {
           "lat": "-58.46463459803104",
-          "lng": "-34.5673114271266",
-          "_id": "65cf79667c7f00be703574af"
+          "lng": "-34.5673114271266"
       },
       "materiales": [
           "RAEES",
@@ -20,72 +20,61 @@
           "Tetra-brik",
           "Telgopor"
       ],
-      "dia_hora": "Martes a Sábado de 11 a 17 hs.",
-      "hora": "8 a 20",
-      "__v": 0
+      "horario_atencion": "Martes a Sábado de 11 a 17 hs."
   },
   {
       "_id": "65cf7e7b78eeb6a60d976adb",
       "nombre": "Punto de Reciclaje 2",
+      "ubicacion": "Avenida La Ensaladera 658",
       "imagen": "https://ejemplo.com/imagen.jpg",
       "latLng": {
           "lat": "-58.42475759495435",
-          "lng": "-34.589092613878925",
-          "_id": "65cf7e7b78eeb6a60d976adc"
+          "lng": "-34.589092613878925"
       },
       "materiales": [
           "vidrio"
       ],
-      "dia_hora": "Martes a Sábado de 11 a 17 hs.",
-      "hora": "8 a 20",
-      "__v": 0
+      "horario_atencion": "Martes a Sábado de 11 a 17 hs."
   },
   {
       "_id": "65cf7f5fdb0c5b95c0518d8e",
       "nombre": "Punto de Reciclaje 3",
+      "ubicacion": "Avenida Chicharrita 568",
       "imagen": "https://ejemplo.com/imagen.jpg",
       "latLng": {
           "lat": "-58.46837082181539",
-          "lng": "-34.54704645232918",
-          "_id": "65cf7f5fdb0c5b95c0518d8f"
+          "lng": "-34.54704645232918"
       },
       "materiales": [
           "vidrio"
       ],
-      "dia_hora": "Martes a Sábado de 11 a 17 hs.",
-      "hora": "8 a 20",
-      "__v": 0
+      "horario_atencion": "Martes a Sábado de 11 a 17 hs."
   },
   {
       "_id": "65cf841529c708db9b06beb6",
       "nombre": "Punto de Reciclaje 4",
+      "ubicacion": "Avenida El Sereno Sin Apuro 1230",
       "imagen": "https://ejemplo.com/imagen.jpg",
       "latLng": {
           "lat": "-58.483418715328696",
-          "lng": "-34.62961178326025",
-          "_id": "65cf841529c708db9b06beb7"
+          "lng": "-34.62961178326025"
       },
       "materiales": [
           "tapitas"
       ],
-      "dia_hora": "Martes a Sábado de 11 a 17 hs.",
-      "hora": "8 a 20",
-      "__v": 0
+      "horario_atencion": "Martes a Sábado de 11 a 17 hs."
   },
   {
-      "_id": "65cf841529c708db9b06beb6",
       "nombre": "Punto de Reciclaje 5",
+      "ubicacion": "Avenida El Cahito 11",
       "imagen": "https://ejemplo.com/imagen.jpg",
       "latLng": {
           "lat": "-58.51645115231643",
-          "lng": "-34.6494611405422",
-          "_id": "65cf841529c708db9b06beb7"
+          "lng": "-34.6494611405422"
       },
       "materiales": [
           "RAEES / Aceite vegetal usado (en botellas cerradas) / Pilas/ Papel / Cartón / Vidrio / Metal / Plástico / Tetra-brik / Telgopor"
       ],
-      "dia_hora": "Martes a Sábado de 11 a 17 hs.",
-      "hora": "8 a 20",
-      "__v": 0
+      "horario_atencion": "Martes a Sábado de 11 a 17 hs."
   }
 ]
