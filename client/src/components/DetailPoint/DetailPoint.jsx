ca374e5d73c0ffd200847c56a0ee4f6abf645708
rretta <caracido@gmail.com>
2024-03-02 04:29:52 -0300

    feature: styled map & detailed points

--- client/src/components/DetailPoint/DetailPoint.jsx
+++ client/src/components/DetailPoint/DetailPoint.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchDataFronJson } from '../../helpers/fetchDataFromJson';
 import { DetailMap } from '../DetailMap/DetailMap';
-
 const DetailPoint = () => {
   const { id } = useParams();
   const [pointData, setPointData] = useState({});
